b75c0543a1d58e55dcd694a5eb95f0fe7eec2eae
novayo <f14051172@gs.ncku.edu.tw>
2019-08-10 16:55:24 +0800

    Update Q2 over 90%

--- 0002_Add_Two_Numbers/README.md
+++ 0002_Add_Two_Numbers/README.md
@@ -1,3 +1,10 @@
-Runtime: 12 ms, faster than 85.87% of C online submissions for Add Two Numbers.
+Runtime: 72 ms, faster than 91.46% of Python3 online submissions for Add Two Numbers.
 
-Memory Usage: 9.1 MB, less than 26.35% of C online submissions for Add Two Numbers.
+Memory Usage: 14 MB, less than 5.67% of Python3 online submissions for Add Two Numbers.
+
+---
+Question: https://leetcode.com/problems/add-two-numbers/
+
+For Try_1.c: https://leetcode.com/submissions/detail/248526450/
+
+For Try_2.py: https://leetcode.com/submissions/detail/250423913/
---
+++ 0002_Add_Two_Numbers/try_2.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ans = tmp = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            
+            tmp.next = ListNode(carry%10)
+            tmp = tmp.next
+            carry //= 10
+        return ans.next
+        

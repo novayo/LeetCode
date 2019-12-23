a7cfcf2b6ddbbe39d9ad9f700d71ea9d1d36db5d
novayo <f14051172@gs.ncku.edu.tw>
2019-12-23 11:35:14 +0800

    unFinish Q208

---
+++ 0208_Implement_Trie_Prefix_Tree/README.md
@@ -0,0 +1,9 @@
+Question: https://leetcode.com/problems/implement-trie-prefix-tree/
+
+---
+
+try_1.py:
+* Runtime: 196 ms, faster than 52.80% of Python3 online submissions for Implement Trie (Prefix Tree).
+* Memory Usage: 31.4 MB, less than 7.41% of Python3 online submissions for Implement Trie (Prefix Tree).
+
+> using Trie
---
+++ 0208_Implement_Trie_Prefix_Tree/try_1.py
@@ -0,0 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.finish = False
+        
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.finish = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for w in word:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return node.finish
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for w in prefix:
+            node = node.children.get(w)
+            if not node:
+                return False
+        return True
+        
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)

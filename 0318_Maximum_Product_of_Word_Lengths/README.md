ec1d6c878bd6680c46cf4032f0585ad819a166fc
novayo <f14051172@gs.ncku.edu.tw>
2020-02-15 16:56:54 +0800

    Finish Q318

--- 0318_Maximum_Product_of_Word_Lengths/README.md
+++ 0318_Maximum_Product_of_Word_Lengths/README.md
@@ -7,3 +7,13 @@ try_1.py: O(n^2)
 * Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Maximum Product of Word Lengths.
 
 > bit
+
+---
+
+try_2.py: O(n^2)
+* Runtime: 184 ms, faster than 96.49% of Python3 online submissions for Maximum Product of Word Lengths.
+* Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Maximum Product of Word Lengths.
+
+> bit + hashmap
+	> 因為相同的文字會有相同的轉換過的bit數字
+	> 假設"a" 跟 "aaaaaaaaa"，會有一樣的數字，所以只要記得"aaaaaaaa"就好了（因為他比較長），這樣記錄下來，去比較的時候，雖然一樣是n^2，但是會有比較少的n
---
+++ 0318_Maximum_Product_of_Word_Lengths/try_2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        d = {"a": 2**0, "b": 2**1, "c": 2**2, "d": 2**3, "e": 2**4, "f": 2**5, "g": 2**6,
+             "h": 2**7, "i": 2**8, "j": 2**9, "k": 2**10, "l": 2**11, "m": 2**12, "n": 2**13,
+             "o": 2**14, "p": 2**15, "q": 2**16, "r": 2**17, "s": 2**18, "t": 2**19, "u": 2**20,
+             "v": 2**21, "w": 2**22, "x": 2**23, "y": 2**24, "z": 2**25}
+        
+        length = collections.defaultdict(int)
+        for i, word in enumerate(words):
+            n = 0
+            tmp = set()
+            for w in word:
+                if w not in tmp:
+                    tmp.add(w)
+                    n += d[w]
+            words[i] = n
+            length[words[i]] = max(length[words[i]], len(word))
+        
+        ans = 0
+        for i in length:
+            for j in length:
+                if i & j == 0:
+                    ans = max(ans, length[i]*length[j])
+        return ans

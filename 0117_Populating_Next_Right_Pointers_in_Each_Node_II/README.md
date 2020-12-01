dcb8f3d3b393b4d13f3221dfab709d049ded0940
ShihChungYu <f14051172@gs.ncku.edu.tw>
2020-12-01 21:57:37 +0800

    Finish Q117

--- 0117_Populating_Next_Right_Pointers_in_Each_Node_II/README.md
+++ 0117_Populating_Next_Right_Pointers_in_Each_Node_II/README.md
@@ -7,3 +7,11 @@ try_1.py: O(nodes) O(1)
 * Memory Usage: 14.8 MB, less than 100.00% of Python3 online submissions for Populating Next Right Pointers in Each Node II.
 
 > iteration (based on Q116)
+
+---
+
+try_2.py: O(nodes) O(n)
+* Runtime: 44 ms, faster than 87.72% of Python3 online submissions for Populating Next Right Pointers in Each Node II.
+* Memory Usage: 15.5 MB, less than 8.49% of Python3 online submissions for Populating Next Right Pointers in Each Node II.
+
+> the same idea as LC116
---
+++ 0117_Populating_Next_Right_Pointers_in_Each_Node_II/try_2.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        
+        queue = collections.deque()
+        queue.append((root, 0))
+        
+        left = None
+        lLevel = -1
+        while queue:
+            right, rLevel = queue.popleft()
+            
+            if rLevel == lLevel:
+                left.next = right
+            
+            left = right
+            lLevel = rLevel
+            
+            if left.left:
+                queue.append((left.left, lLevel+1))
+            if left.right:
+                queue.append((left.right, lLevel+1))
+        
+        return root    

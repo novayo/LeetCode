b94ce54b86e693c6ebd9e2b363242eaaa554d443
ShihChungYu <f14051172@gs.ncku.edu.tw>
2021-11-08 20:24:36 +0800

    [DP_II] Day 12

---
+++ 1055_Shortest_Way_to_Form_String/try_2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        target_processed = self.Preprocess(source, target)
+        if target_processed == -1:
+            return -1
+
+        ans, pre = 1, -1
+        for i in target_processed:
+            if i <= pre:
+                ans += 1
+            pre = i
+        return ans
+
+    def Preprocess(self, source, target):
+        s = set(source)
+
+        target_index = []
+        i=0
+        for t in target:
+            if t not in s:
+                return -1
+
+            while t != source[i]:
+                i+=1
+                if i >= len(source):
+                    i = 0
+            target_index.append(i)
+            i += 1
+            if i >= len(source):
+                i = 0
+        return target_index
